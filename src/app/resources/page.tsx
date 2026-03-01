64c3d3620073ab5e304f2faf433c065002340261
JettN <122491434+JettN@users.noreply.github.com>
2026-02-28 18:15:04 -0800

    setup initial code for each page

---
+++ src/app/resources/page.tsx
@@ -0,0 +1,10 @@
+import styles from "../../styles/resources.module.css";
+
+export default function ProjectsPage() {
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.title}>Projects</h1>
+      <p>Project list goes here.</p>
+    </main>
+  );
+}
