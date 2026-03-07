9bf08e981d791a87f977b094b45e2b382e55afe7
LinaSeto <lseto@ucsd.edu>
2026-03-07 13:01:16 -0800

    create outline for projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,78 @@
 import styles from "../../styles/projects.module.css";
+import Link from 'next/link';
 
 export default function ProjectsPage() {
   return (
     <main className={styles.container}>
-      <h1 className={styles.title}>Projects</h1>
-      <p>Project list goes here.</p>
+      <h1 className={styles.page_title}>Project Entries</h1>
+      <form action="/search" method="get">
+        <input type="search" name="q" placeholder="Search keywords..."/>
+        <button>Search</button>
+      </form>
+      <section className="current projects">
+        <h2 className={styles.title}>Active and Planned Projects</h2>
+        <ul className={styles.grid}>
+          <li>
+            <Link href='projects/indiv_project'>
+              <article className={styles.card}>
+                <img alt='Project Preview'/>
+                <h3>Project Title</h3>
+                <p>Group Members</p>
+              </article>
+            </Link>
+          </li>
+          <li>
+            <Link href='projects/indiv_project'>
+              <article className={styles.card}>
+                <img alt='Project Preview'/>
+                <h3>Project Title</h3>
+                <p>Group Members</p>
+              </article>
+            </Link>
+          </li>
+          <li>
+            <Link href='projects/indiv_project'>
+              <article className={styles.card}>
+                <img alt='Project Preview'/>
+                <h3>Project Title</h3>
+                <p>Group Members</p>
+              </article>
+            </Link>
+          </li>
+        </ul>
+      </section>
+      <section className="past projects">
+        <h2 className={styles.title}>Past Projects</h2>
+        <ul className={styles.grid}>
+          <li>
+            <Link href='projects/indiv_project'>
+              <article className={styles.card}>
+                <img alt='Project Preview'/>
+                <h3>Project Title</h3>
+                <p>Group Members</p>
+              </article>
+            </Link>
+          </li>
+          <li>
+            <Link href='projects/indiv_project'>
+              <article className={styles.card}>
+                <img alt='Project Preview'/>
+                <h3>Project Title</h3>
+                <p>Group Members</p>
+              </article>
+            </Link>
+          </li>
+          <li>
+            <Link href='projects/indiv_project'>
+              <article className={styles.card}>
+                <img alt='Project Preview'/>
+                <h3>Project Title</h3>
+                <p>Group Members</p>
+              </article>
+            </Link>
+          </li>
+        </ul>
+      </section>
     </main>
   );
 }
