1cbe8f335e6c75d6c2e920fd5d7fe35683c06972
JettN <122491434+JettN@users.noreply.github.com>
2026-03-07 14:33:58 -0800

    added base content to home page

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default async function Home() {
+
+  return (
+    <div>
+      <main style={{ padding: '2rem' }}>
+        <h1>Welcome to the Projects Portal</h1>
+        
+      </main>
+    </div>
+  );
+}
---
+++ src/app/projects/[slug]/page.tsx
@@ -0,0 +1,11 @@
+export default async function Entry() {
+
+  return (
+    <div>
+      <main style={{ padding: '2rem' }}>
+        <h1>Welcome to the Projects Portal</h1>
+        
+      </main>
+    </div>
+  );
+}
